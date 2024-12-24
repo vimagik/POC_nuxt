2fb784a6c0d49401196d953ce96b242bf68f412c
Georgiy Rzhavin <g.rzhavin@yandex.ru>
2024-12-24 15:45:21 +0000

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<script setup>
+const { login } = useDirectusAuth();
+
+async function onSubmit() {
+    try {
+        await login({ email: "admin@example.com", password: "d1r3ctu5" });
+        console.log("Залогинились")
+    } catch (e) { }
+};
+</script>
+
+<template>
+    <div>
+        <q-card dark bordered class="bg-grey-9 my-card">
+            <q-card-section>
+                <div class="text-h6">Our Changing Planet</div>
+            </q-card-section>
+
+            <q-card-section class="q-pt-none">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua.
+            </q-card-section>
+
+            <q-separator dark inset />
+
+            <q-card-section>
+                <q-btn @click="onSubmit" color="white" text-color="black">Логин</q-btn>
+            </q-card-section>
+        </q-card>
+    </div>
+</template>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 250px
+</style>
