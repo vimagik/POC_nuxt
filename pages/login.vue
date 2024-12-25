898e32fd5e39cd6e361900f083f79fbd990b0372
Georgiy Rzhavin <g.rzhavin@yandex.ru>
2024-12-25 19:33:50 +0000

    Update login form

--- pages/login.vue
+++ pages/login.vue
@@ -8,19 +8,30 @@ definePageMeta({
 
 const email = ref('')
 const password = ref('')
+const loadingStatus = ref(false)
+const errorLogin = ref(false)
 
 async function onSubmit() {
+    errorLogin.value = false
+    loadingStatus.value = true
     try {
         await login({ email: email.value, password: password.value });
         router.push('/')
-    } catch (e) { }
+    } catch (e) {
+        errorLogin.value = true
+    }
+    loadingStatus.value = false
 }
 </script>
 
 <template>
     <div class="main row justify-center items-center">
         <q-card class="my-card">
-            <img src="https://cdn.quasar.dev/img/mountains.jpg">
+            <q-img src="https://cdn.quasar.dev/img/mountains.jpg">
+                <div class="absolute-bottom row justify-center">
+                    <img src="/public/img/JDE.webp">
+                </div>
+            </q-img>
 
             <q-card-section>
                 <div class="text-h6 text-center">Авторизация</div>
@@ -28,12 +39,13 @@ async function onSubmit() {
 
             <q-card-section class="q-pt-none">
                 <q-form @submit="onSubmit">
-                    <q-input v-model="email" label="Логин" lazy-rules
+                    <q-input v-model="email" @change="errorLogin = false" label="Логин" lazy-rules
+                        error-message="Ошибка при вводе логина или пароля" :error="errorLogin"
                         :rules="[val => val && val.length > 0 || 'Обязательное поле']">
                     </q-input>
-                    <q-input v-model="password" label="Пароль" lazy-rules
+                    <q-input v-model="password" type="password" label="Пароль" lazy-rules
                         :rules="[val => val && val.length > 0 || 'Обязательное поле']"></q-input>
-                    <q-btn label="Войти" type="submit" color="primary" />
+                    <q-btn class="q-mt-sm" label="Войти" type="submit" color="primary" :loading="loadingStatus" />
                 </q-form>
             </q-card-section>
         </q-card>
@@ -43,6 +55,7 @@ async function onSubmit() {
 <style lang="sass" scoped>
 .main
     height: 100vh
+    background-color: #f7eedc 
 
 .my-card
     width: 100%
