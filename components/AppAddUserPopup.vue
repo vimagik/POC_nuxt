659c45924d1f34a91bb8cff4f134de4abeb4c5be
Georgiy Rzhavin <g.rzhavin@yandex.ru>
2024-12-28 20:01:22 +0000

    Added form

--- components/AppAddUserPopup.vue
+++ components/AppAddUserPopup.vue
@@ -1,21 +1,41 @@
 <script setup>
 const addUser = defineModel()
+
+const addUserData = ref({
+    name: '',
+    action: '',
+    progress: 0,
+    step: ''
+})
 </script>
 
 <template>
     <q-dialog v-model="addUser" persistent>
         <q-card style="min-width: 350px">
             <q-card-section>
-                <div class="text-h6">Your address</div>
+                <div class="text-h6">Новый пользователь</div>
             </q-card-section>
 
             <q-card-section class="q-pt-none">
-                <q-input dense autofocus @keyup.enter="addUser = false" />
+                <q-form>
+                    <q-input autofocus v-model="addUserData.name" @keyup.enter="addUser = false" label="Имя"
+                        :rules="[val => val && val.length > 0 || 'Обязательное поле']" lazy-rules />
+                    <q-input v-model="addUserData.action" @keyup.enter="addUser = false" label="Последняя активность"
+                        :rules="[val => val && val.length > 0 || 'Обязательное поле']" lazy-rules />
+                    <q-input v-model="addUserData.progress" type="number" @keyup.enter="addUser = false"
+                        label="Прогресс" :rules="[
+                            val => val && val >= 0 || 'Обязательное поле',
+                            val => val <= 100 || 'Значение не может быть больше 100'
+                        ]" lazy-rules />
+                    <q-input v-model="addUserData.step" @keyup.enter="addUser = false" label="Этап"
+                        :rules="[val => val && val.length > 0 || 'Обязательное поле']" lazy-rules />
+                </q-form>
+
             </q-card-section>
 
             <q-card-actions align="right" class="text-primary">
-                <q-btn flat label="Cancel" v-close-popup />
-                <q-btn flat label="Add address" v-close-popup />
+                <q-btn flat label="Отмена" v-close-popup />
+                <q-btn flat label="Создать пользователя" v-close-popup />
             </q-card-actions>
         </q-card>
     </q-dialog>
