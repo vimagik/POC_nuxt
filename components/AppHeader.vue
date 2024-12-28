ad74be1d66ad7888004917a7edb1e1fd19bf73b6
Georgiy Rzhavin <g.rzhavin@yandex.ru>
2024-12-28 11:53:29 +0000

    Refactor default layout

---
+++ components/AppHeader.vue
@@ -0,0 +1,25 @@
+<template>
+    <q-header elevated class="my-header text-white" height-hint="98">
+        <q-toolbar>
+            <q-toolbar-title>
+                <q-img src="/img/JDE.webp" width="150px" height="70px" />
+            </q-toolbar-title>
+            <AppHeaderAbout />
+        </q-toolbar>
+
+        <q-tabs align="left" inline-label>
+            <q-route-tab to="/" icon="home" />
+            <q-icon name="arrow_forward_ios"></q-icon>
+            <q-route-tab to="/summary" label="Результаты" icon="assignment_turned_in" />
+            <q-icon name="arrow_forward_ios"></q-icon>
+            <q-route-tab to="/design" label="Редактирование" icon="design_services" />
+        </q-tabs>
+    </q-header>
+</template>
+
+<style lang="sass" scoped>
+.my-header 
+    background-color: #3f3a31 
+.logo 
+    width: 150px 
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,6 @@
 <template>
     <q-layout class="main">
-        <q-header elevated class="my-header text-white" height-hint="98">
-            <q-toolbar>
-                <q-toolbar-title>
-                    <img class="logo" src="/public/img/JDE.webp">
-                </q-toolbar-title>
-                <AppHeaderAbout />
-            </q-toolbar>
-
-            <q-tabs align="left" inline-label>
-                <q-route-tab to="/" icon="home" />
-                <q-icon name="arrow_forward_ios"></q-icon>
-                <q-route-tab to="/summary" label="Результаты" icon="assignment_turned_in" />
-                <q-icon name="arrow_forward_ios"></q-icon>
-                <q-route-tab to="/design" label="Редактирование" icon="design_services" />
-            </q-tabs>
-        </q-header>
+        <AppHeader />
         <q-page-container>
             <slot />
         </q-page-container>
@@ -23,10 +8,6 @@
 </template>
 
 <style lang="sass" scoped>
-.my-header 
-    background-color: #3f3a31 
-.logo 
-    width: 150px 
 .main 
     background-color: #f7eedc
 </style>
